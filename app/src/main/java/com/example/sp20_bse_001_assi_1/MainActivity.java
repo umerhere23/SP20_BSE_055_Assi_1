8af53c8bf0c40d95203d50f3fa6decdaf40f8344
Muhammad Umer Mukhtiar <92437033+sp20bse055@users.noreply.github.com>
2022-04-21 19:55:25 +0500

    upload

--- app/src/main/java/com/example/sp20_bse_001_assi_1/MainActivity.java
+++ app/src/main/java/com/example/sp20_bse_001_assi_1/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.sp20_bse_001_assi_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+private Button btn1,btn2,btn3,btn4,btn5,btn6, btn7,btn8,btn9,btn0,btnclear,btnopen;
+private TextView code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
