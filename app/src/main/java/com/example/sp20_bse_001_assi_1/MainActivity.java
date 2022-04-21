d55fdb954096bf12d673d947f3835140d4525f9a
Muhammad Umer Mukhtiar <92437033+sp20bse055@users.noreply.github.com>
2022-04-21 20:14:07 +0500

    upload

--- app/src/main/java/com/example/sp20_bse_001_assi_1/MainActivity.java
+++ app/src/main/java/com/example/sp20_bse_001_assi_1/MainActivity.java
@@ -3,16 +3,43 @@ package com.example.sp20_bse_001_assi_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 private Button btn1,btn2,btn3,btn4,btn5,btn6, btn7,btn8,btn9,btn0,btnclear,btnopen;
-private TextView code;
+private TextView Code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1.findViewById(R.id.btn1);
+        btn2.findViewById(R.id.btn2);
+        btn3.findViewById(R.id.btn3);
+        btn4.findViewById(R.id.btn4);
+        btn5.findViewById(R.id.btn5);
+        btn6.findViewById(R.id.btn6);
+        btn7.findViewById(R.id.btn7);
+        btn8.findViewById(R.id.btn8);
+        btn9.findViewById(R.id.btn9);
+        btn0.findViewById(R.id.btn0);
+        btnclear.findViewById(R.id.btnopen);
+        btnopen.findViewById(R.id.btnopen);
+
+btn1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Code.setText(Code.getText()+"1");
     }
+});
+btn2.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Code.setText(Code.getText()+"2");
+    }
+});
+    }
+
 }
