9011cdfae641ab8aeebe5574e76f0f2449cd0014
Muhammad Umer Mukhtiar <92437033+sp20bse055@users.noreply.github.com>
2022-04-21 19:45:24 +0500

    upload

---
+++ app/src/main/java/com/example/sp20_bse_001_assi_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sp20_bse_001_assi_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
